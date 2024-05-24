5de31d112392b4335d6ad0e8f1531e04f8e7f9ab
harshal099 <harshal.agrahari.1914@gmail.com>
2024-05-24 14:07:31 +0530

    Minor error cprrection in apierror

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,15 +1,15 @@
 class ApiError extends Error{
-    constructor{
+    constructor(
         statusCode,
         message = "Something went wrong",
         errors = [],
         stack = ""
-    }{
+    ){
         super(message)
-        this.statusCode = statusCode,
-        this.data = null,
-        this.message = message,
-        this.success = false,
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false
         this.errors = this.errors
         
         if(stack){
